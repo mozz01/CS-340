f25567260c7e0a6a7f7fce8d55f406d1bacf216e
Saurav Shrestha <96402139+shresaur@users.noreply.github.com>
2024-02-03 20:28:12 -0500

    SQL for insert queries

---
+++ schema/DDL.sql
@@ -0,0 +1,15 @@
+-- Insert data in Stores table
+INSERT INTO Stores(name, phone, address) VALUES ('Metropolitan Stories', '402-547-6787', '1 Broad Court, Irmo, SC 29063');
+INSERT INTO Stores(name, phone, address) VALUES ('Literary Books', '542-888-2167', '121 Main Lane, Pataskala, OH 43062');
+INSERT INTO Stores(name, phone, address) VALUES ('Bookshelf Boutique', '901-325-4321', '9906 W. Union Street, Burke, VA 22015');
+
+-- Insert data in Customers table
+INSERT INTO Customers(firstName, lastName, email, phone) VALUES ('Patricia', 'Carlson', 'pcarlson@gmail.com', '667-222-4521');
+INSERT INTO Customers(firstName, lastName, email, phone) VALUES ('Selena', 'Lozada', 'slozada@gmail.com', '701-548-1944');
+INSERT INTO Customers(firstName, lastName, email, phone) VALUES ('Michael', 'Karlsson', 'mkarlsson@gmail.com', '402-666-1234');
+INSERT INTO Customers(firstName, lastName, email, phone) VALUES ('Sergio', 'Hernandez', 'shernandez@gmail.com', '801-951-3574');
+
+-- Insert data in Authors table
+INSERT INTO Authors(firstName, lastName) VALUES ('Edgar', 'Patterson');
+INSERT INTO Authors(firstName, lastName) VALUES ('William', 'Fitzgerald');
+INSERT INTO Authors(firstName, lastName) VALUES ('Geroge', 'Wolf');
