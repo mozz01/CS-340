7bbcfa3f1c9c1d86efaf8d8ea62154028ec03dad
Error-Mozz13 <mozz7e6@gmail.com>
2024-02-06 20:36:05 -0800

    Formatted and fixed all insert statements

--- ProjectGroup21_step2_DRAFT/DDL.sql
+++ ProjectGroup21_step2_DRAFT/DDL.sql
@@ -4,6 +4,7 @@
 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+-- SET FOREIGN_KEY_CHECKS=0;
 SET AUTOCOMMIT = 0;
 
 -- -----------------------------------------------------
@@ -116,41 +117,128 @@ ENGINE = InnoDB;
 -- ------------------------------------------------------------------------------------
 
 -- Insert data in Stores table
-INSERT INTO Stores(name, phone, address) VALUES ('Metropolitan Stories', '402-547-6787', '1 Broad Court, Irmo, SC 29063');
-INSERT INTO Stores(name, phone, address) VALUES ('Literary Books', '542-888-2167', '121 Main Lane, Pataskala, OH 43062');
-INSERT INTO Stores(name, phone, address) VALUES ('Bookshelf Boutique', '901-325-4321', '9906 W. Union Street, Burke, VA 22015');
+INSERT INTO Stores (NAME, phone, address)
+VALUES
+  (
+    'Metropolitan Stories',
+    '402-547-6787',
+    '1 Broad Court, Irmo, SC 29063'
+  ),
+  (
+    'Literary Books',
+    '542-888-2167',
+    '121 Main Lane, Pataskala, OH 43062'
+  ),
+  (
+    'Bookshelf Boutique',
+    '901-325-4321',
+    '9906 W. Union Street, Burke, VA 22015'
+  );
 
 -- Insert data in Customers table
-INSERT INTO Customers(firstName, lastName, email, phone) VALUES ('Patricia', 'Carlson', 'pcarlson@gmail.com', '667-222-4521');
-INSERT INTO Customers(firstName, lastName, email, phone) VALUES ('Selena', 'Lozada', 'slozada@gmail.com', '701-548-1944');
-INSERT INTO Customers(firstName, lastName, email, phone) VALUES ('Michael', 'Karlsson', 'mkarlsson@gmail.com', '402-666-1234');
-INSERT INTO Customers(firstName, lastName, email, phone) VALUES ('Sergio', 'Hernandez', 'shernandez@gmail.com', '801-951-3574');
+INSERT INTO Customers (firstName, lastName, email, phone)
+VALUES
+  (
+    'Patricia',
+    'Carlson',
+    'pcarlson@gmail.com',
+    '667-222-4521'
+  ),
+  (
+    'Selena',
+    'Lozada',
+    'slozada@gmail.com',
+    '701-548-1944'
+  ),
+  (
+    'Michael',
+    'Karlsson',
+    'mkarlsson@gmail.com',
+    '402-666-1234'
+  ),
+  (
+    'Sergio',
+    'Hernandez',
+    'shernandez@gmail.com',
+    '801-951-3574'
+  );
 
 -- Insert data in Authors table
-INSERT INTO Authors(firstName, lastName) VALUES ('Edgar', 'Patterson');
-INSERT INTO Authors(firstName, lastName) VALUES ('William', 'Fitzgerald');
-INSERT INTO Authors(firstName, lastName) VALUES ('George', 'Wolf');
-INSERT INTO Authors(firstName, lastName) VALUES ('Mike', 'Lang');
-
+INSERT INTO Authors (firstName, lastName)
+VALUES
+  ('Edgar', 'Patterson'),
+  ('William', 'Fitzgerald'),
+  ('George', 'Wolf'),
+  ('Mike', 'Lang');
 
 -- Insert data in Books table
-INSERT INTO Books(title, yearOfPublication, price) VALUES ('Linear Algebra', '2005', 80.00);
-INSERT INTO Books(title, yearOfPublication, price) VALUES ('Discrete Mathematics', '1992', 90.99);
-INSERT INTO Books(title, yearOfPublication, price) VALUES('Organic Chemistry', '2020', 120.00);
-INSERT INTO Books(title, yearOfPublication, price) VALUES('Statistics', 2021, 115.00);
-
-
+INSERT INTO Books (title, yearofpublication, price)
+VALUES
+  ('Linear Algebra', '2005', 80.00),
+  ('Discrete Mathematics', '1992', 90.99),
+  ('Organic Chemistry', '2020', 120.00),
+  ('Statistics', 2021, 115.00);
 
 -- Insert data in AuthorsBooks intersection table
-INSERT INTO AuthorsBooks(bookID, authorID) VALUES ((SELECT bookID FROM Books WHERE title = 'Linear Algebra'), (SELECT authorID FROM Authors WHERE firstName = 'Edgar' and lastName = 'Patterson'));
-INSERT INTO AuthorsBooks(bookID, authorID) VALUES ((SELECT bookID FROM Books WHERE title = 'Linear Algebra'), (SELECT authorID FROM Authors WHERE firstName = 'William' and lastName = 'Fitzgerald'));
-INSERT INTO AuthorsBooks(bookID, authorID) VALUES ((SELECT bookID FROM Books WHERE title = 'Discrete Mathematics'), (SELECT authorID FROM Authors WHERE firstName = 'William' and lastName = 'Fitzgerald'));
-INSERT INTO AuthorsBooks(bookID, authorID) VALUES ((SELECT bookID FROM Books WHERE title = 'Organic Chemistry'), (SELECT authorID FROM Authors WHERE firstName = 'George' and lastName = 'Wolf'));
-INSERT INTO AuthorsBooks(bookID, authorID) VALUES ((SELECT bookID FROM Books WHERE title = 'Statistics'), NULL);
+INSERT INTO AuthorsBooks (bookID, authorID)
+VALUES
+(
+  (
+    SELECT bookID FROM Books WHERE title = 'Linear Algebra'
+  ),
+  (
+    SELECT authorID
+    FROM Authors
+    WHERE firstName = 'Edgar'
+      AND lastName = 'Patterson'
+  )
+),
+(
+  (
+    SELECT bookID FROM Books WHERE title = 'Linear Algebra'
+  ),
+  (
+    SELECT authorID
+    FROM Authors
+    WHERE firstName = 'William'
+      AND lastName = 'Fitzgerald'
+  )
+),
+(
+  (
+    SELECT bookID FROM Books WHERE title = 'Discrete Mathematics'
+  ),
+  (
+    SELECT authorID
+    FROM Authors
+    WHERE firstName = 'William'
+      AND lastName = 'Fitzgerald'
+  )
+),
+(
+  (
+    SELECT bookID FROM Books WHERE title = 'Organic Chemistry'
+  ),
+  (
+    SELECT authorID
+    FROM Authors
+    WHERE firstName = 'George'
+      AND lastName = 'Wolf'
+  )
+),
+(
+  (
+    SELECT bookID FROM Books WHERE title = 'Statistics'
+  ),
+  NULL
+);
+
 
 -- ------------------------------------------------------------------------------------
 
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+-- SET FOREIGN_KEY_CHECKS=1;
 COMMIT;
+
