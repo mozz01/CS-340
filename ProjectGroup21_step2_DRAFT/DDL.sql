87f5178decc6687c80047d18c6aceed2b8100e41
Error-Mozz13 <mozz7e6@gmail.com>
2024-02-06 19:54:24 -0800

    Refactored clean importing statements

--- ProjectGroup21_step2_DRAFT/DDL.sql
+++ ProjectGroup21_step2_DRAFT/DDL.sql
@@ -1,14 +1,10 @@
 -- Group #21 - Mo Hudeihed & Saurav Shrestha
 -- MySQL Workbench Forward Engineering
 
-SET FOREIGN_KEY_CHECKS=0;
-SET AUTOCOMMIT = 0;
-
--- ------------------------------------------------------------------------------------
-
 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+SET AUTOCOMMIT = 0;
 
 -- -----------------------------------------------------
 -- Table `Books`
@@ -115,10 +111,9 @@ CREATE TABLE IF NOT EXISTS `AuthorsBooks` (
 ENGINE = InnoDB;
 
 
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
-
+-- ------------------------------------------------------------------------------------
+-- INSERT DATA SECTION
+-- ------------------------------------------------------------------------------------
 
 -- Insert data in Stores table
 INSERT INTO Stores(name, phone, address) VALUES ('Metropolitan Stories', '402-547-6787', '1 Broad Court, Irmo, SC 29063');
@@ -154,5 +149,8 @@ INSERT INTO AuthorsBooks(bookID, authorID) VALUES ((SELECT bookID FROM Books WHE
 INSERT INTO AuthorsBooks(bookID, authorID) VALUES ((SELECT bookID FROM Books WHERE title = 'Statistics'), NULL);
 
 -- ------------------------------------------------------------------------------------
-SET FOREIGN_KEY_CHECKS=1;
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
 COMMIT;
