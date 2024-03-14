2f414031631e5d72033c6c5a02fd91300a23b437
Saurav Shrestha <96402139+shresaur@users.noreply.github.com>
2024-03-13 20:42:39 -0400

    fixed

--- Step 6/ProjectGroup21_step6_FINAL/Website/public/js/authors.js
+++ Step 6/ProjectGroup21_step6_FINAL/Website/public/js/authors.js
@@ -14,7 +14,7 @@ const addButton = document.querySelector(".addButton");
 
 document.addEventListener('DOMContentLoaded', function() {
     addFirstName.addEventListener('input', function() {
-        const enteredText = this.value;
+        const enteredText = this.value.trim();
         const maxLength = this.getAttribute('maxlength');
 
         if ((enteredText.length <= 0) || (enteredText.length > maxLength)) {
